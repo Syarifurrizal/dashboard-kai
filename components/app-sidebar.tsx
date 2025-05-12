7b872baec998a31bed86c657d1f3bf6a0f701996
Syarifurrizal <130261635+Syarifurrizal@users.noreply.github.com>
2025-05-12 19:28:31 +0700

    feat: navigation

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import {
+  BadgeDollarSign,
+  ChartCandlestick,
+  ShieldAlert,
+  ShieldCheck,
+  Wrench,
+} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+import { Separator } from "./ui/separator"
+import Image from "next/image"
+
+import Logo from "@/public/KAI.svg"
+import { SidebarDropdown } from "./SidebarDropdown"
+
+const kdg = [
+
+  { title: "Availability", url: "/kdg/availability", icon: ShieldCheck },
+  { title: "Perawatan", url: "/kdg/perawatan", icon: Wrench },
+  { title: "Saldo Gudang", url: "/kdg/saldogudang", icon: BadgeDollarSign },
+  { title: "Balance Movement", url: "/kdg/balancemovement", icon: ChartCandlestick },
+  { title: "Gangguan", url: "/kdg/gangguan", icon: ShieldAlert },
+]
+
+const kdk = [
+
+  { title: "Availability", url: "/kdk/availability", icon: ShieldCheck },
+  { title: "Perawatan", url: "/kdk/perawatan", icon: Wrench },
+  { title: "Saldo Gudang", url: "/kdk/saldogudang", icon: BadgeDollarSign },
+  { title: "Balance Movement", url: "/kdk/balancemovement", icon: ChartCandlestick },
+  { title: "Gangguan", url: "/kdk/gangguan", icon: ShieldAlert },
+]
+
+const kdt = [
+
+  { title: "Availability", url: "/kdt/availability", icon: ShieldCheck },
+  { title: "Perawatan", url: "/kdt/perawatan", icon: Wrench },
+  { title: "Saldo Gudang", url: "/kdt/saldogudang", icon: BadgeDollarSign },
+  { title: "Balance Movement", url: "/kdt/balancemovement", icon: ChartCandlestick },
+  { title: "Gangguan", url: "/kdt/gangguan", icon: ShieldAlert },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <Link href="/" className="flex flex-col items-center justify-center pt-4 pb-1 gap-2">
+          <Image src={Logo} width={125} priority alt="Logo KAI" />
+          <h1 className="text-center font-lg font-bold text-lg">SARANA DAOP 6</h1>
+        </Link>
+
+        <Separator />
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>Application</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu>
+
+            <SidebarDropdown label="KDK" items={kdk} />
+            <SidebarDropdown label="KDT" items={kdt} />
+            <SidebarDropdown label="KDG" items={kdg} />
+
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
